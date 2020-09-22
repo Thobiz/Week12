2cdff3c91583af775e21ca1c248885d1233c85e7
Thobiz <69186131+Thobiz@users.noreply.github.com>
2020-09-22 11:42:53 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+   Project : if-else,while,Loop,switch
+   Authorities:Mr.Jirayoot
+   Date:22/09/63
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"borke.h"
+int main() {
+    printf("===Main Program==\n");
+    int mtqq();
+      ifelse();
+      grade();
+    return 0;
+}
